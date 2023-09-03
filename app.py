e84c4aacd0cd9ccbf41cd65c234b94e49c233df4
rishu <divyanshkumarworks@gmail.com>
2023-09-04 00:51:11 +0530

    fixed import issue in app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ def home():
 
 import controller.search_controller as search_controller
 import controller.movies_controller as movies_controller
-import controller.user_register as user_register_controller
-import controller.user_authenticate as user_authenticate_controller
+import controller.user_register_controller as user_register_controller
+import controller.user_authenticate_controller as user_authenticate_controller
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
